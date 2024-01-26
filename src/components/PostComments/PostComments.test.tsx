905085e6407ca258335210c350a4a8c5098cf13a
Gustavo Botti <gustavo.l.botti@viannasempre.com.br>
2024-01-26 13:35:33 -0300

    resolução

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -4,7 +4,27 @@ import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Deve inserir novos comentários', () => {
+        const commentText = 'Testando...';
+        const anotherCommentText = 'Outro comentário';
+    
+        render(<PostComment />);
+
+        screen.debug()
+
+        fireEvent.change(screen.getByTestId('comment-textarea'), { target: { value: commentText } });
+        fireEvent.click(screen.getByText('Comentar'));
+
+        fireEvent.change(screen.getByTestId('comment-textarea'), { target: { value: anotherCommentText } });
+        fireEvent.click(screen.getByText('Comentar'));
+
+        screen.debug()
+    
+        expect(screen.getByText(commentText)).toBeInTheDocument();
+        expect(screen.getByText(anotherCommentText)).toBeInTheDocument();
+      });
 });
